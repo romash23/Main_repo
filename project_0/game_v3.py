f47d4bb7d4c7347cd3ad04e4ca51205bbb7e0eb0
romash23 <romaryukov@gmail.com>
2024-12-04 19:17:33 +0400

    edit readme.md

--- project_0/game_v3.py
+++ project_0/game_v3.py
@@ -19,7 +19,9 @@ def game_core_v3(number: int = 1) -> int:
             predict = predict//2
     #print(f'Текущее число {number}: количество попыток - {count}')
     return count
-lst = []
-for number in range(1,101):  
-    lst.append(game_core_v3(number))
-print('Максимальное число попыток равно', max(lst))  
+def max_attempt_score(me_core_v3):
+    lst = []
+    for number in range(1,101):  
+        lst.append(game_core_v3(number))
+    #print('Максимальное число попыток равно', max(lst))  
+    return max(lst)
